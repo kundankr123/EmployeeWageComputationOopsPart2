fd653f4cc424b686d6e73383feb5cbdae3973ed5
kundan <goswamikundan123@gmail.com>
2023-02-23 15:38:20 +0530

    Added: UseCase9 Files which is to compute total wage for each company and saves it.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeWageComputationOopsPart2;
+
+namespace EmployeeWageComputationOoopsPart2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            EmpWageBuilderUC9 capGemini = new EmpWageBuilderUC9("CapGemini", 15, 5, 17);
+            EmpWageBuilderUC9 Wipro = new EmpWageBuilderUC9("Wipro", 16, 3, 21);
+
+            Wipro.computeEmpWage();
+            Console.WriteLine(Wipro.toString());
+            capGemini.computeEmpWage();
+            Console.WriteLine(capGemini.toString());
+        }
+    }
+}
